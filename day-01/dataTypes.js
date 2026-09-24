21f53f90dc5785fc61a6d18f22ad2de775efbb87
code-with-shanto <abulbashershanto1@gmail.com>
2026-09-24 23:48:51 +0600

    datatype done

---
+++ day-01/dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hellow") // we r using node js not browser
+
+console.log(3
+    +
+    3) // codde readability should be high
+console.log('shanto')
+
+
+let name = "shanto"
+let age = 21
+let isloggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/flase
+// null => standalone value
+// indefined =>
+// symbol = unique
+
+// object
+
+console.log(typeof null)  //object
+console.log(typeof undefined)  //undefined
