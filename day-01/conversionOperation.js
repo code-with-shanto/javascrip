e0246082449921c91f9bc632e34527d12b33d329
code-with-shanto <abulbashershanto1@gmail.com>
2026-09-25 00:05:10 +0600

    conversion of data types

---
+++ day-01/conversionOperation.js
@@ -0,0 +1,20 @@
+let score = 'shanto'
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score) //string to value
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+//"33"= 33
+//"33asc"= NaN
+
+let isloggedIn = 1
+
+let boleanisLoggedIn = Boolean(isloggedIn)
+console.log(boleanisLoggedIn)
+
+//,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
