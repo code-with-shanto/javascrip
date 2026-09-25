d3677768158cb39028492abfbd1ba2e8021fc366
code-with-shanto <abulbashershanto1@gmail.com>
2026-09-25 10:17:14 +0600

    operations

---
+++ day-02/operations.js
@@ -0,0 +1,25 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+
+let str1 = "hello"
+let str2 = "shanto"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+// gamecounter++;
+++gamecounter;
+console.log(gamecounter)
+
+
