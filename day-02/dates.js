700c8cc565770d848d2eb80e3e7bfde495c770c4
code-with-shanto <abulbashershanto1@gmail.com>
2026-09-25 23:17:25 +0600

    dats

---
+++ day-02/dates.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleTimeString())
+
+let myCreatedDate = new Date(2005, 7,7)
+console.log(myCreatedDate.toDateString());
